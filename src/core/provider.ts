f3fb6799f95318aea5c09a3608a5dea047ebb61c
Danylo Tolmachov <danylo.tolmachov@gmail.com>
2024-08-30 18:21:49 +0200

    Fetch events in specific period

--- src/core/provider.ts
+++ src/core/provider.ts
@@ -8,15 +8,22 @@ import { CategoryKeywords } from "./dto/category-keywords";
 export abstract class Provider {
   protected _providerName!: string;
   protected _providerUrl!: string;
-  protected yesterdayDate: Date;
+  protected yesterdayStartDate: Date;
+  protected todayStartDate: Date;
   protected minPubDate: Date
+  protected maxPubDate: Date
 
   constructor() {
-    this.yesterdayDate = new Date();
-    this.yesterdayDate.setDate(this.yesterdayDate.getDate() - 1);
+    this.yesterdayStartDate = new Date();
+    this.yesterdayStartDate.setDate(this.yesterdayStartDate.getDate() - 1);
+    this.yesterdayStartDate.setHours(0, 0, 0, 0);
+
+    this.todayStartDate = new Date();
+    this.todayStartDate.setHours(0, 0, 0, 0);
 
     // process?.env?.minPubDate - ISOString. E.g. 2024-08-30T13:28:12.634Z
-    this.minPubDate = process?.env?.minPubDate ? new Date(process.env.minPubDate) : this.yesterdayDate;
+    this.minPubDate = process?.env?.minPubDate ? new Date(process.env.minPubDate) : this.yesterdayStartDate;
+    this.maxPubDate = process?.env?.maxPubDate ? new Date(process.env.maxPubDate) : this.todayStartDate;
   }
 
   public async fetch(): Promise<GrowthEventDto[]> {
@@ -27,35 +34,22 @@ export abstract class Provider {
 
     try {
       const feed = await parser.parseURL(this._providerUrl);
-
-      console.log(`Fetched ${feed.items.length} items from ${this._providerName}`);
-
       const parsedItems = feed.items.map(item => item as unknown as RssItemDto);
-      console.log("Parsed items:", parsedItems.length);
-
-      const dateFilteredItems = parsedItems.filter(this.dateFilter.bind(this));
-      console.log("Date filtered items:", dateFilteredItems.length);
-
+      const providerFilteredEvents = parsedItems.filter(this.providerFilter.bind(this));
+      const dateFilteredItems = providerFilteredEvents.filter(this.dateFilter.bind(this));
       const mappedEvents = dateFilteredItems.map(this.buildGrowthEvent.bind(this));
-      console.log("Mapped events:", mappedEvents.length);
-
       const filteredEvents = mappedEvents.filter(event => event !== null);
-      console.log("Filtered events:", filteredEvents.length);
-
       const categorizedEvents = filteredEvents.filter(event => event.category !== Category.Unknown);
-      console.log("Categorized events:", categorizedEvents.length);
 
-      const providerFilteredEvents = categorizedEvents.filter(this.providerFilter);
-      console.log("Provider filtered events:", providerFilteredEvents.length);
-
-      events = events.concat(providerFilteredEvents);
+      events = events.concat(categorizedEvents);
     } catch (error) {
       console.error(`Error fetching ${this._providerName} RSS feed:`, error);
     }
 
-    console.log(`Fetched ${events.length} events from ${this._providerName}`);
-
-    return events;
+    const orderedEvents = events.sort((a, b) => (new Date(a.date) as any) - (new Date(b.date) as any));
+    console.log(`Fetched ${orderedEvents.length} events from ${this._providerName}`);
+    
+    return orderedEvents;
   }
 
   public buildGrowthEvent(items: RssItemDto): GrowthEventDto | null {
@@ -64,10 +58,11 @@ export abstract class Provider {
 
   public dateFilter(item: RssItemDto): boolean {
     if (!item.pubDate) { return false; }
-    return new Date(item.pubDate) >= this.minPubDate;
+    const pubDate = new Date(item.pubDate);
+    return pubDate >= this.minPubDate && pubDate <= this.maxPubDate;
   }
 
-  public providerFilter(events: GrowthEventDto): boolean {
+  public providerFilter(item: RssItemDto): boolean {
     return true;
   }
 
